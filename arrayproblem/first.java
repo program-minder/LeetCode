28c05103c015012daeaf15a977b14bb6eabee328
program-minder <santhoshkumarcse2005@gmail.com>
2025-12-06 23:04:16 +0530

    this is my first problem

---
+++ arrayproblem/first.java
@@ -0,0 +1,8 @@
+public class first {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        for (int num : arr) {
+            System.out.println(num);
+        }
+    }
+}
