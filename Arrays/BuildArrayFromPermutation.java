56935ce4fb7196f2e122a1f6e18f439a5a385b9a
program-minder <santhoshkumarcse2005@gmail.com>
2025-12-07 13:02:52 +0530

    first program

---
+++ Arrays/BuildArrayFromPermutation.java
@@ -0,0 +1,12 @@
+// Problem and sollution link: https://leetcode.com/problems/build-array-from-permutation/solutions/7396451/leetcode-1920-build-array-from-permutati-oy2s/
+
+public class BuildArrayFromPermutation {
+    public int[] buildArray(int[] nums) {
+       
+        int [] res=new int[nums.length];
+        for(int i=0;i<nums.length;i++){
+            res[i]=nums[nums[i]];
+        }
+        return res;
+    }
+}
